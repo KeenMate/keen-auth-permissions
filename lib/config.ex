ce205090b10c1ec5f55aff1ad95ff8c70818c26c
Filip Jakab <jakabfilip.fj@gmail.com>
2022-06-15 23:50:48 +0200

    Rewritten user post-processing and added some specs'n'docs

--- lib/config.ex
+++ lib/config.ex
@@ -1,5 +1,14 @@
 defmodule KeenAuthPermissions.Config do
+  @moduledoc """
+  ## Configuration
+  This package requires `db_context` to be provided with module name pointing to the generated ecto_gen `DbContext`
+  which contains following introspected stored procedures:
+
+  - ensure_user(username, email, display_name, provider)
+  - update_roles_and_permissions()
+  """
+
   def get_db_context() do
-    Application.get_env(:keen_auth_permissions, :db_context)
+    Application.fetch_env!(:keen_auth_permissions, :db_context)
   end
 end
--- lib/processor.ex
+++ lib/processor.ex
@@ -7,16 +7,38 @@ defmodule KeenAuthPermissions.Processor do
   def process(conn, provider, %{user: user} = oauth_response) do
     db_context = Config.get_db_context()
 
-    with {:ok, db_user} <- db_context.ensure_user(user["preferred_username"], user["email"], provider),
-         {:ok, %{roles: roles, permissions: permissions}} <- db_context.update_roles_and_permissions(db_user.user_id) do
-      oauth_response =
-        Map.update!(
-          oauth_response,
-          :user,
-          &Map.merge(&1, %{user_id: db_user.user_id, roles: roles, permissions: permissions})
-        )
+    with {:ok, db_user} <- ensure_user(db_context, user, provider),
+         {:ok, roles_and_permissions} <- update_user_roles(db_context, user, db_user, provider),
+         user <- enrich_user(user, db_user, roles_and_permissions) do
+      oauth_response = Map.put(oauth_response, :user, user)
 
       {:ok, conn, oauth_response}
+    end
+  end
+
+  def enrich_user(user, db_user, roles_and_permissions) do
+    user
+    |> Map.put(:user_id, db_user.user_id)
+    |> Map.merge(roles_and_permissions)
+  end
+
+  def update_user_roles(db_context, _user, db_user, _provider) do
+    case db_context.update_roles_and_permissions(db_user.user_id) do
+      {:ok, [roles_and_permissions]} ->
+        {:ok, roles_and_permissions}
+
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  def ensure_user(db_context, user, provider) do
+    case db_context.ensure_user(user["preferred_username"], user["email"], user["display_name"], provider) do
+      {:ok, [user]} ->
+        {:ok, user}
+
+      {:error, reason} ->
+        {:error, reason}
     end
   end
 end
