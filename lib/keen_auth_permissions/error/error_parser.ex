69466851b24ee51f1ab853af99ae6226734fc5f1
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2022-10-09 17:41:43 +0200

    parse messages from database error

--- lib/keen_auth_permissions/error/error_parser.ex
+++ lib/keen_auth_permissions/error/error_parser.ex
@@ -16,108 +16,91 @@ defmodule KeenAuthPermissions.Error.ErrorParsers do
 
   def parse_error(%Postgrex.Error{
         postgres: %{
-          pg_code: code
+          pg_code: code,
+          message: msg
         }
       }) do
     {code, _} = Integer.parse(code, 10)
-    parse_error(code)
+    {:error, create(parse_reason(code), msg)}
   end
 
-  def parse_error(50003), do: create(:permission_denied, "Permission denied")
+  def parse_reason(50003), do: :permission_denied
 
   # * USERS
-  def parse_error(52101), do: create(:cannot_ensure_user, "Cannot ensure user for email provider")
+  def parse_reason(52101),
+    do: :cannot_ensure_user
 
-  def parse_error(52102),
-    do: create(:identity_used, "User cannot register user because the identity is already in use")
+  def parse_reason(52102),
+    do: :identity_used
 
-  def parse_error(52103), do: create(:user_doesnt_exist, "User does not exist")
+  def parse_reason(52103), do: :user_doesnt_exist
 
-  def parse_error(52104), do: create(:user_is_system, "User is a system user")
-  def parse_error(52105), do: create(:user_inactive, "User is in inactive state")
+  def parse_reason(52104), do: :user_is_system
+  def parse_reason(52105), do: :user_inactive
 
-  def parse_error(52106), do: create(:user_locked, "User is locked")
+  def parse_reason(52106), do: :user_locked
 
-  def parse_error(52107), do: create(:provider_inactive, "Provider is not active")
+  def parse_reason(52107), do: :provider_inactive
 
-  def parse_error(52108), do: create(:user_not_in_tenant, "User has no access to tenant")
+  def parse_reason(52108), do: :user_not_in_tenant
 
-  def parse_error(52109), do: create(:no_permission, "User has no correct permission in tenant")
+  def parse_reason(52109), do: :no_permission
 
-  def parse_error(52110), do: create(:identity_inactive, "User provider identity is not active")
+  def parse_reason(52110), do: :identity_inactive
 
-  def parse_error(52111),
-    do: create(:identity_doesnt_exist, "User provider identity does not exist")
+  def parse_reason(52111),
+    do: :identity_doesnt_exist
 
-  def parse_error(52112), do: create(:login_disabled, "User is not supposed to log in")
+  def parse_reason(52112), do: :login_disabled
 
   # * GROUPS
-  def parse_error(52171), do: create(:group_not_found, "User group not found")
+  def parse_reason(52171), do: :group_not_found
 
-  def parse_error(52172),
-    do: create(:group_inactive, "User cannot be added to group because the group is not active")
+  def parse_reason(52172),
+    do: :group_inactive
 
-  def parse_error(52173),
-    do:
-      create(
-        :group_unassignable_or_external,
-        "User cannot be added to group because it's either not assignable or an external group"
-      )
+  def parse_reason(52173),
+    do: :group_unassignable_or_external
 
-  def parse_error(52174),
-    do: create(:object_id_and_role_undefined, "Either mapped object id or role must not be empty")
+  def parse_reason(52174),
+    do: :object_id_and_role_undefined
 
   # * PERMISSIONS
-  def parse_error(52175),
-    do: create(:permission_set_unassignable, "Permission set is not assignable")
+  def parse_reason(52175),
+    do: :permission_set_unassignable
 
-  def parse_error(52176), do: create(:permission_unassignable, "Permission is not assignable")
+  def parse_reason(52176), do: :permission_unassignable
 
-  def parse_error(52177),
-    do: create(:permission_undefined, "Permission set is not defined in tenant")
+  def parse_reason(52177),
+    do: :permission_undefined
 
-  def parse_error(52178),
-    do: create(:some_permission_unassignable, "Some permission is not assignable")
+  def parse_reason(52178),
+    do: :some_permission_unassignable
 
-  def parse_error(52271),
-    do:
-      create(
-        :cant_delete_system_group,
-        "User group cannot be deleted because it's a system group"
-      )
+  def parse_reason(52271),
+    do: :cant_delete_system_group
 
-  def parse_error(52272),
-    do:
-      create(
-        :group_and_targe_user_undefined,
-        "Either user group id or target user id has to be not null"
-      )
+  def parse_reason(52272),
+    do: :group_and_targe_user_undefined
 
-  def parse_error(52273),
-    do:
-      create(
-        :permission_and_permission_set_undefined,
-        "Either permission set code or permission code has to be not null "
-      )
+  def parse_reason(52273),
+    do: :permission_and_permission_set_undefined
 
-  def parse_error(52274),
-    do:
-      create(:permission_id_and_code_undefined, "Either permission id or code has to be not null")
+  def parse_reason(52274),
+    do: :permission_id_and_code_undefined
 
-  def parse_error(52275), do: create(:permission_doesnt_exist, "Permission does not exist")
+  def parse_reason(52275), do: :permission_doesnt_exist
 
   # * TOKENS
-  def parse_error(52276), do: create(:token_used, "The same token is already used")
+  def parse_reason(52276), do: :token_used
 
-  def parse_error(52277), do: create(:token_doesnt_exist, "Token does not exist")
+  def parse_reason(52277), do: :token_doesnt_exist
 
-  def parse_error(52278),
-    do: create(:token_invalid_or_expired, "Token is not valid or has expired")
+  def parse_reason(52278),
+    do: :token_invalid_or_expired
 
-  def parse_error(52279), do: create(:token_wrong_user, "Token was created for different user")
+  def parse_reason(52279), do: :token_wrong_user
 
   # * FALLBACK
-  def parse_error(code), do: IO.inspect(code, label: "unknown error code")
-
-  create(:unknown, "Unknown error code")
+  def parse_reason(code), do: :unknown
 end
