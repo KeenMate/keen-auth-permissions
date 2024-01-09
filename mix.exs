d26587ccb4f880107001fe874d49a59027611fe2
Filip Jakab <jakabfilip.fj@gmail.com>
2024-01-09 17:14:24 +0100

    Added new required information for publishing this package

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule KeenAuthPermissions.MixProject do
       version: "0.1.1",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -21,11 +23,30 @@ defmodule KeenAuthPermissions.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:keen_auth, "~> 0.2.2"},
       {:jason, "~> 1.3"},
       {:postgrex, "~> 0.16.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description() do
+    """
+    Library that extends base `keen_auth` by providing necessary logic for permissions handling
+    """
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "keen_auth_permissions",
+      # organization: "keenmate",
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README.md LICENSE),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/KeenMate/keen_auth_permissions"}
     ]
   end
 end
